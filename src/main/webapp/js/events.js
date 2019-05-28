8d7b1ea374b8e3568b8434f05978466999480b58
Tiago Silva <tiago.silva@cronapp.io>
2019-05-28 19:09:38 +0000

    ultimo commit, eu acho

--- src/main/webapp/js/events.js
+++ src/main/webapp/js/events.js
@@ -1,10 +1,8 @@
 $(document).ready(function() {
     setTimeout(function() {
         $('.awards-slide').slick({
-            infinite: true,
+            infinite: false,
             arrows: false,
-            dots: true,
-            initialSlide: 1,
             slidesToShow: 3,
             slidesToScroll: 3,
              responsive: [
@@ -12,8 +10,8 @@ $(document).ready(function() {
                     breakpoint: 1024,
                     settings: {
                         slidesToShow: 3,
+                        initialSlide: 1,
                         slidesToScroll: 3,
-                        infinite: true,
                         dots: true
                     }
                 },
@@ -21,14 +19,18 @@ $(document).ready(function() {
                     breakpoint: 600,
                     settings: {
                         slidesToShow: 2,
-                        slidesToScroll: 2
+                        initialSlide: 1,
+                        slidesToScroll: 2,
+                        dots: true,
                     }
                 },
                 {
                     breakpoint: 480,
                     settings: {
                         slidesToShow: 1,
-                        slidesToScroll: 1
+                        initialSlide: 1,
+                        slidesToScroll: 1,
+                        dots: true,
                     }
               }
             ]
