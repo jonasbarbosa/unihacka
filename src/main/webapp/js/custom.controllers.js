8d8a62ba0a403727c386943c6f65a330734d0f8d
Tiago Silva <tiago.silva@cronapp.io>
2019-05-30 15:23:38 +0000

    Alteração do mapa e menus de sessão

--- src/main/webapp/js/custom.controllers.js
+++ src/main/webapp/js/custom.controllers.js
@@ -1,11 +1,24 @@
-app.controller('AfterLoginController', function($scope) {
- //
-});
+app.controller('ScrollController', function($scope, $location, $anchorScroll) {
+    $scope.links = [
+        {
+            name: 'Como Funciona',
+            href: 'how-work'
+        }, 
+        {
+            name: 'O Desafio',
+            href: 'challenge'
+        }, 
+        {
+            name: 'Prêmios',
+            href: 'awards'
+        }, 
+        {
+            name: 'Local',
+            href: 'local'
+        }
+    ];
 
-app.controller('AfterHomeController', function($scope) {
- //
-});
-
-app.controller('AfterPageController', function($scope) {
- //
+    $scope.goToSection = (id) => {
+        $("html, body").animate({ scrollTop: $(`#${id}`).offset().top - 100 }, 1000);
+    };
 });
